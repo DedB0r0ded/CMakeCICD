c86e506d53f352d6e9c9ebc709679d6ed32d69bf
DedB0r0ded <mega.sasha2005@mail.ru>
2024-04-15 22:52:36 +0300

    actions

--- Tests/ListTests.cpp
+++ Tests/ListTests.cpp
@@ -2,15 +2,13 @@
 #include "../List.h"
 
 template <typename T>
-std::shared_ptr<List<T>> makeList_test() {
+std::shared_ptr<List<T>> makeMyList_test() {
 	std::shared_ptr<List<T>> l = std::make_shared<List<int>>();
 	return l;
 }
 
-#define MKL makeList_test<int>
-auto l = MKL();
-
 TEST(ListTests, BasicAssertions) {
+	auto l = makeMyList_test<int>();
   EXPECT_NE(l, nullptr);
 	ASSERT_TRUE(l->empty());
 	l->push_back(10);
