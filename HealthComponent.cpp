735ba3d633a24dba5de4484303b9423713eefba3
DedB0r0ded <mega.sasha2005@mail.ru>
2024-04-20 00:27:28 +0300

    Added HealthComponent implementation

--- HealthComponent.cpp
+++ HealthComponent.cpp
@@ -1,1 +1,56 @@
 #include "HealthComponent.h"
+
+bool HealthComponent::_updateIsDead()
+{
+  if (m_health > 0)
+    m_isDead = false;
+  else
+    m_isDead = true;
+
+  return m_isDead;
+}
+
+HealthComponent::HealthComponent(const int maxHealth, const int health) : m_maxHealth{ maxHealth }, m_health { health }, m_isDead{ false }
+{
+  if (m_health > m_maxHealth)
+    m_health = m_maxHealth;
+  _updateIsDead();
+}
+
+int HealthComponent::takeDamage(const int damage)
+{
+  int appliedDamage;
+  
+  if (m_health - damage > 0)
+    appliedDamage = damage;
+  else
+    appliedDamage = m_health;
+  
+  m_health -= appliedDamage;
+  _updateIsDead();
+
+  return appliedDamage;
+}
+
+int HealthComponent::heal(const int healValue)
+{
+  int appliedHeal;
+
+  if (healValue <= 0)
+    return 0;
+
+  if (healValue <= m_maxHealth - m_health)
+    appliedHeal = healValue;
+  else
+    appliedHeal = m_maxHealth - m_health;
+
+  m_health += appliedHeal;
+  _updateIsDead();
+
+  return appliedHeal;
+}
+
+bool HealthComponent::isDead() const
+{
+  return m_isDead;
+}
--- HealthComponent.h
+++ HealthComponent.h
@@ -1,3 +1,16 @@
 #include "Component.h"
 
-class HealthComponent : public Component{};
+class HealthComponent : public Component{
+  int m_maxHealth;
+  int m_health;
+  bool m_isDead;
+
+  bool _updateIsDead();
+
+public:
+  HealthComponent(const int maxHealth = 0, const int health = 0);
+
+  int takeDamage(const int damage);
+  int heal(const int healValue);
+  bool isDead() const;
+};
