8f36fd938cf353987832d2b7a148166c456f9c95
DedB0r0ded <revikad.00@mail.ru>
2024-06-17 02:53:28 +0300

    enums

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,46 @@
 ﻿#include "Header.h"
 
+enum struct Color : int {Cyan, Magenta, Yellow, Black};
+
+std::istream& operator>>(std::istream& is, Color& color) {
+	std::underlying_type_t<Color> input;
+	is >> input;
+	switch ((Color)input) {
+		case Color::Cyan:			color = Color(input); break;
+		case Color::Magenta:	color = Color(input); break;
+		case Color::Yellow:		color = Color(input); break;
+		case Color::Black:		color = Color(input); break;
+		default:			std::cout << "Invalid color input.";
+	}
+	return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Color& color) {
+	switch (color) {
+		case Color::Cyan:			os << "Cyan"; break;
+		case Color::Magenta:	os << "Magenta"; break;
+		case Color::Yellow:		os << "Yellow"; break;
+		case Color::Black:		os << "Black"; break;
+		default:							std::cout << "Unknown color.";
+	}
+	return os;
+}
+
 int main(int argc, char** argv)
 {
-	auto falsePredicate = []() -> bool { return false; };
-	auto truePredicate = []() { return true; };
+	int x{ 0 };
+	auto l = [&x]() {
+		std::cout << (x += 100) << std::endl;
+		};
 
-	IDamagable* e = new Entity();
-	std::cout << e->takeDamageIf(10, falsePredicate) << std::endl;
-	std::cout << e->takeDamageIf(10, truePredicate) << std::endl;
-	delete e;
+	l();
+	l();
+
+	std::underlying_type_t<Color> a;
+	a = 2;
+
+	std::cout << Color::Cyan;
+
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
